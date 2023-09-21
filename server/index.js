5c28fe0756fffbf6a206d17d1d70b6eec56eb5e8
Wisdom okorie <91771635+Star-ddog@users.noreply.github.com>
2023-09-21 11:24:39 -0400

    Update index.js

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.use(cors(
 	{
-		origin:["https://closesellerss-gb.vercel.app"],
+		origin:["https://closeseller-front.vercel.app"],
 		methods:["POST", "GET"],
 		credentials:true
 	}
